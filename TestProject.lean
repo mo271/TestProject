ce8f86bd1375c9ac21b570d34d40b51bbd8b382b
Moritz Firsching <firsching@google.com>
2024-04-21 17:55:05 +0200

    add Erdős.lean

--- TestProject.lean
+++ TestProject.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `TestProject` library.
 -- Import modules here that should be built as part of the library.
 import «TestProject».Basic
+import «TestProject».«Erdős»
---
+++ TestProject/Erdős.lean
@@ -0,0 +1,1 @@
+theorem test : 1 + 1 = 2 := rfl
