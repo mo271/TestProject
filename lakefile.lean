a2bff3e517f97eca4c4d3714352c290a8307ec20
Moritz Firsching <firsching@google.com>
2024-04-21 17:44:16 +0200

    initial commit

---
+++ lakefile.lean
@@ -0,0 +1,20 @@
+import Lake
+open Lake DSL
+
+package «TestProject» where
+  -- Settings applied to both builds and interactive editing
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`pp.proofs.withType, false⟩
+  ]
+  -- add any additional package configuration options here
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
+
+@[default_target]
+lean_lib «TestProject» where
+  -- add any library configuration options here
